bf5ad89d63301c0381bd31a2836de9f06c922c90
Varun Ram S <80676450+varunram2001@users.noreply.github.com>
2023-08-10 22:46:15 +0530

    Add files via upload

---
+++ Logic Gate - NAND Gate/testbench.sv
@@ -0,0 +1,23 @@
+// Code your testbench here
+// or browse Examples
+module nand_gate_tb;
+reg a,b;
+wire y;
+
+nand_gate_s uut(a,b,y);
+
+initial begin
+  $dumpfile("dump.vcd");
+    $dumpvars(1);
+a = 0; b = 0;
+#10
+b = 0; b = 1;
+#10
+a = 1; b = 0;
+#10
+b = 1; b = 1;
+#10
+$finish();
+end
+                
+endmodule
