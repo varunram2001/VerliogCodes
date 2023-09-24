6dedb2de982160d8c9d91ff171663aaf5a2b72b7
Varun Ram S <80676450+varunram2001@users.noreply.github.com>
2023-09-24 09:00:17 +0530

    Add files via upload

---
+++ Johnson Counter/design.sv
@@ -0,0 +1,15 @@
+module johnson_counter (
+  input wire clk, // Clock input
+  input wire reset, // Reset input
+  output reg [3:0] count // 4-bit counter output
+);
+
+  always @(posedge clk or posedge reset) begin
+    if (reset) begin
+      count <= 4'b0001;
+    end else begin
+      count <= {count[2:0], ~count[3]};
+    end
+  end
+
+endmodule
