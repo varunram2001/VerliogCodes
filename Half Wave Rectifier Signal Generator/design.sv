b1731fb6fc00df451b96626bd8951715f7781ee7
Varun Ram S <80676450+varunram2001@users.noreply.github.com>
2023-10-19 11:20:23 +0530

    Add files via upload

---
+++ Half Wave Rectifier Signal Generator/design.sv
@@ -0,0 +1,15 @@
+module half_wave_rectifier (
+  input wire clk, // Clock input
+  input wire [7:0] sine_wave, // 8-bit sine wave input
+  output reg [7:0] rectified_wave // 8-bit rectified output
+);
+
+  always @(posedge clk) begin
+    if (sine_wave >= 8'b10000000) begin
+      rectified_wave <= sine_wave - 8'b10000000;
+    end else begin
+      rectified_wave <= 8'b00000000;
+    end
+  end
+
+endmodule
